e65f17b7fafff4fcb5c2c63c09b20ac11d7b4087
PlastusMaximus <tillomat42@icloud.com>
2025-11-14 23:39:24 +0100

    Refactoring

--- Assets/02_Scripts/PlayerControls.cs
+++ Assets/02_Scripts/PlayerControls.cs
@@ -1,4 +1,10 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
 using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerControls : MonoBehaviour
 {
@@ -6,27 +12,137 @@ public class PlayerControls : MonoBehaviour
     private GameObject flashlight;
     private bool flashlightActive;
     
+    private GameObject flashCam;
+    private bool flashCamActive;
+    
+    public Camera playerCam;
+    private CharacterController characterController;
+    private bool canMove = true;
+    
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    public float defaultHeight = 2f;
+    public float crouchHeight = 1f;
+    public float crouchSpeed = 3f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        flashlight.gameObject.SetActive(false);    
+        flashlight.gameObject.SetActive(false);  
+        characterController = GetComponent<CharacterController>();
+        lockCursor();
     }
 
     // Update is called once per frame
     void Update()
     {
+        flashlightControls();
+        
+        float movementDirectionY = moveDirection.y;
+        
+        setMovementDirection(movementDirectionY);
+        handleJumping(movementDirectionY);
+        handleGravity();
+        handleCrouching();
+        handleMovement();
+        
+    }
+
+    private void lockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void flashlightControls()
+    {
         if (Input.GetKeyDown(KeyCode.F))
+                {
+                    if (flashlightActive == false)
+                    {
+                        flashlight.gameObject.SetActive(true);
+                        flashlightActive = true;
+                    }
+                    else
+                    {
+                        flashlight.gameObject.SetActive(false);
+                        flashlightActive = false;
+                    }
+                }
+    }
+
+    private void setMovementDirection(float movementDirectionY)
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+    }
+
+    private void handleJumping(float movementDirectionY)
+    {
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            if (flashlightActive == false)
-            {
-                flashlight.gameObject.SetActive(true);
-                flashlightActive = true;
-            }
-            else
-            {
-                flashlight.gameObject.SetActive(false);
-                flashlightActive = false;
-            }
+            moveDirection.y = jumpPower;
+        } 
+        else
+        {
+            moveDirection.y = movementDirectionY;
         }
     }
+    
+    private void handleGravity()
+    {
+        if (!characterController.isGrounded)
+
+        {
+
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        }
+    }
+
+    private void handleCrouching()
+    {
+        if (Input.GetKey(KeyCode.R) && canMove)
+        {
+            characterController.height = crouchHeight;
+            walkSpeed = crouchSpeed;
+            runSpeed = crouchSpeed;
+        }
+        else
+        {
+
+            characterController.height = defaultHeight;
+            walkSpeed = 6f;
+            runSpeed = 12f;
+
+        }
+    }
+
+    private void handleMovement()
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+
 }
